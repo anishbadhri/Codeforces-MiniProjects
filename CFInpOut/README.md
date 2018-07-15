a4e5c637ea86f2adaee5f3d5baeaeffdd6b01d46
anishbadhri <anishbadhri@gmail.com>
2018-07-15 12:05:14 +0530

    Added CFInpOut

---
+++ CFInpOut/README.md
@@ -0,0 +1,23 @@
+# Codeforces Input and Output
+A simple python program which scrapes the input and output for a given question from Codeforces
+
+## Installation
+To install Python3, run <br>
+```
+sudo apt-get update
+sudo apt-get install python3.6
+```
+To install pip, run<br>
+```
+sudo apt install python3-pip
+```
+Install the requirements for the Python file with <br>
+```
+pip install -r requirements.txt
+```
+
+## Running the File
+Run the file with <br>
+```
+python3 WebScrape.py
+```
---
+++ CFInpOut/WebScrape.py
@@ -0,0 +1,24 @@
+from bs4 import BeautifulSoup
+import requests
+import os
+import sys
+if len(sys.argv) != 2:
+	print("Please Refer README.md for Syntax")
+	exit(1)
+ques = sys.argv[1]
+num = ques[:len(ques)-1]
+dif = ques[len(ques)-1]
+page_link = 'http://codeforces.com/problemset/problem/'+num+'/'+dif
+print(page_link)
+page_response = requests.get(page_link, timeout=10)
+page_content = BeautifulSoup(page_response.content, "html.parser")
+if len(page_content.find_all("div",attrs={"class":"input"})) > 0:
+	os.mkdir("./"+ques)
+for i in range(0, len(page_content.find_all("div",attrs={"class":"input"}))):
+	paragraphs = page_content.find_all("div",attrs={"class":"input"})[i].pre.get_text(strip=True,separator="\n")
+	file = open(ques+"/"+ques+"Inp"+str(i+1)+'.in',"w+")
+	file.write(paragraphs)
+for i in range(0, len(page_content.find_all("div",attrs={"class":"output"}))):
+	paragraphs = page_content.find_all("div",attrs={"class":"output"})[i].pre.get_text(strip=True,separator="\n")
+	file = open(ques+"/"+ques+"Out"+str(i+1)+'.out',"w+")
+	file.write(paragraphs)
